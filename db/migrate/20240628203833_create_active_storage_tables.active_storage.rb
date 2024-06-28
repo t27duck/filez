5a541f2a5fd8e25dea18312190fbcd8abd978a24
Tony Drake <t27duck@gmail.com>
2024-06-28 16:41:56 -0400

    Lint

--- db/migrate/20240628203833_create_active_storage_tables.active_storage.rb
+++ db/migrate/20240628203833_create_active_storage_tables.active_storage.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This migration comes from active_storage (originally 20170806125915)
 class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
   def change
@@ -33,7 +35,8 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
         t.datetime :created_at, null: false
       end
 
-      t.index [ :record_type, :record_id, :name, :blob_id ], name: :index_active_storage_attachments_uniqueness, unique: true
+      t.index [ :record_type, :record_id, :name, :blob_id ], name: :index_active_storage_attachments_uniqueness,
+        unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
 
